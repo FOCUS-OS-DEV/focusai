b9da9c82391cd614a9c8a293f287aa77f560fd26
FOCUS-OS-DEV <oneil@focusai.co.il>
2025-12-20 02:21:32 +0200

    fix: add images remotePatterns and remove fileURLToPath

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -4,7 +4,6 @@ import { headers as getHeaders } from 'next/headers.js'
 import Image from 'next/image'
 import { getPayload } from 'payload'
 import React from 'react'
-import { fileURLToPath } from 'url'
 
 import config from '@/payload.config'
 import './styles.css'
@@ -14,8 +13,6 @@ export default async function HomePage() {
   const payloadConfig = await config
   const payload = await getPayload({ config: payloadConfig })
   const { user } = await payload.auth({ headers })
-
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
 
   return (
     <div className="home">
@@ -49,12 +46,6 @@ export default async function HomePage() {
             Documentation
           </a>
         </div>
-      </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
       </div>
     </div>
   )
